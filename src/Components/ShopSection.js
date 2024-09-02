13774c3d1b8415ba2570259dd83b95e9d5b0d3bb
Precious <Preshomondi@gmail.com>
2024-09-02 15:34:37 +0300

    Increase height in small screens

--- src/Components/ShopSection.js
+++ src/Components/ShopSection.js
@@ -28,7 +28,7 @@ const ShopSection = () => {
           <img
             src={section.imgSrc}
             alt={section.caption}
-            className="w-full h-64 object-cover rounded-lg lg:h-96 lg:w-full"
+            className="w-full h-80 object-cover rounded-lg lg:h-96 lg:w-full" // Increased height for small screens
           />
           <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white text-center">
             <p>{section.caption}</p>
