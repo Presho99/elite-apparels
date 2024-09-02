17aa45dc0cd06ebf3a7c124ab297596b73a3c431
Precious <Preshomondi@gmail.com>
2024-09-02 14:48:33 +0300

    Enable shop section navigation

--- src/Components/ShopSection.js
+++ src/Components/ShopSection.js
@@ -1,26 +1,26 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import loungewear from '../assets/one.jpg';
 import sale from '../assets/two.jpg';
 import basics from '../assets/one.jpg';
 import tops from '../assets/two.jpg';
 import dresses from '../assets/one.jpg';
-import under10 from '../assets/two.jpg';
+// import under10 from '../assets/two.jpg'; // Uncomment if you decide to use this section
 
 const ShopSection = () => {
   const sections = [
-    { imgSrc: loungewear, caption: 'Shop Loungewear' },
-    { imgSrc: sale, caption: 'Shop Sale' },
-    { imgSrc: basics, caption: 'Shop Basics' },
-    { imgSrc: tops, caption: 'Shop Tops' },
-    { imgSrc: dresses, caption: 'Shop Dresses' },
-    { imgSrc: under10, caption: 'Under $10' }
+    { imgSrc: loungewear, caption: 'Shop new arrivals', link: '/new-arrivals' },
+    { imgSrc: sale, caption: 'Shop basics', link: '/basics' },
+    { imgSrc: basics, caption: 'Shop for women', link: '/women' },
+    { imgSrc: tops, caption: 'Shop for men', link: '/men' },
+    { imgSrc: dresses, caption: 'Shop sale', link: '/sale' },
+    // { imgSrc: under10, caption: 'Under $10', link: '/under10' } // Uncomment if you decide to use this section
   ];
 
   return (
     <div className="mt-8 space-y-8">
       {sections.map((section, index) => (
-        <div key={index} className="relative">
+        <Link key={index} to={section.link} className="block relative cursor-pointer">
           <img
             src={section.imgSrc}
             alt={section.caption}
@@ -29,7 +29,7 @@ const ShopSection = () => {
           <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white text-center">
             <p>{section.caption}</p>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
